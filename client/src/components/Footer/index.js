9b6cfc4c3f79e54c26178c97c797b24246702342
Kelsea Fields <kelsea.fields@yahoo.com>
2021-02-16 12:55:02 -0800

    footer and styling

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+        <span>Kelsea's Portfolio 2021</span>
+      </footer>
+    </div>
+  );
+}
+export default Footer;
