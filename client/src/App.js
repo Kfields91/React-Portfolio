d190a9ced7ca4cc9e5e37894d5ec89f573e6edf2
Kelsea Fields <kelsea.fields@yahoo.com>
2021-02-16 08:58:14 -0800

    adjusting file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
@@ -8,13 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>
+            React Portfolio
+          </h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </div>
     );
   }
 }
